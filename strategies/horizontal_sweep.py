67a74b228a904854bb4386755e33c7ce599424a2
rcox771 <russ771@gmail.com>
2020-02-04 01:31:15 -0700

    dont need atm

--- strategies/horizontal_sweep.py
+++
@@ -1,5 +0,0 @@
-def horizontal_sweep(board):
-    for x,row in enumerate(board):
-        for y,space in enumerate(row):
-            if space not in ["X", "-"]:
-                return x, y
--- strategies/random_shot.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-def random_shot(board):
-    while True:
-        x = randint(0,len(board)-1)
-        y = randint(0,len(board)-1)
-        if board[x][y] not in ["X","-"]:
-            return x,y
