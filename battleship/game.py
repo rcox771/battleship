51427d01c356342f1f0957db9ef3e4b798217959
rcox771 <russ771@gmail.com>
2020-02-15 19:27:25 -0700

    made game respect configuration

--- battleship/game.py
+++ battleship/game.py
@@ -1,28 +1,35 @@
+from __future__ import absolute_import
+
 import random
 import numpy as np
 from matplotlib import pyplot as plt
 from .utils import get_ax_labels, get_short_uuid
+import inspect
+from . import ships
+
+
+def get_ships():
+    d = dict(inspect.getmembers(ships))
+    d = {k: d[k] for k in d if 'ships.' in repr(
+        d[k]).lower() and not k.startswith('__')}
+    return d
 
 
 class Board:
-    def __init__(self, board_size=10, ship_counts=None,
+    def __init__(self, board_size=10,
                  placement_strategy=None):
-        if not ship_counts:
-            ship_counts = config['ship']['ship_counts']
-        self.ship_counts = ship_counts
-        if not placement_strategy:
-            placement_strategy = config['placement']['placement_strategy']
-        self.placement_strategy = placement_strategy
+
         self.board = np.zeros((board_size, board_size), dtype=np.uint8)
         self.rows, self.cols = get_ax_labels(board_size)
         self.ships = []
         self.place_ships(placement_strategy)
 
     def place_ships(self, placement_strategy):
-        for ship_type in self.ship_counts:
-            to_place = self.ship_counts[ship_type]
+        _ships = get_ships()
+        for ship in _ships:
+            to_place = _ships[ship]().count
             for _ in range(to_place):
-                ship = ship_type(id=len(self.ships))
+                ship = _ships[ship](id=len(self.ships))
                 ship.place_on(self.board,
                               placement_strategy=self.placement_strategy)
                 assert ship.is_placed()
