9deb673f179616b79d87190644c11b20437fd411
rcox771 <russ771@gmail.com>
2020-02-17 22:40:42 -0700

    added repurposful config reader

--- battleship/config.py
+++ battleship/config.py
@@ -1,9 +1,11 @@
 import yaml
 from pathlib import Path
 
+config_path = Path().cwd() / 'config.yaml'
+
 
 class Configurable(object):
-    def __init__(self, *args, file=Path().cwd() / 'config.yaml', **kwargs):
+    def __init__(self, *args, file=config_path, **kwargs):
         super().__init__(*args, **kwargs)
         self.file = file
 
@@ -14,11 +16,15 @@ class Configurable(object):
         return config
 
     def read_config(self):
-        with open(self.file) as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-        return config
+        return read_config(self.file)
 
     def __getattr__(self, name):
         """called automatically when an attribute does not exist"""
         base, sup = [x.__name__ for x in self.__class__.__mro__[::-1][-2:]]
         return self.get_value(base, sup, name)
+
+
+def read_config(path=config_path):
+    with open(path) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    return config
